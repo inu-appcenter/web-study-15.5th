ad552ccbb32172573c225f8519a5787773cb7cce
So Jeong <7658641@naver.com>
2023-11-30 15:15:27 +0900

    FEAT: 새로운 채팅 추가 버튼 구현

--- 2-4weaks_study/sidebar/src/ChatList.tsx
+++ 2-4weaks_study/sidebar/src/ChatList.tsx
@@ -1,71 +1,41 @@
-import React, { useState } from "react";
-import { Icons } from "./Icons";
-
-const chatItems=[
-  {
-    title: "react에서 list 렌더링하는법",
-    chatId: "3",
-    lastChat: new Date().getTime(),
-  },
-  {
-    title: "react 장점",
-    chatId: "2",
-    lastChat: new Date().getTime() - 3600 * 24 * 3,
-  },
-  {
-    title: "react JSX",
-    chatId: "1",
-    lastChat: new Date().getTime() - 3600 * 24 * 8,
-  },
-  {
-    title: "react 프레임워크",
-    chatId: "0",
-    lastChat: new Date().getTime() - 3600 * 24 * 9,
-  },
-];
-
+import React, { useState } from 'react';
+import { Icons } from './Icons';
 
 const ChatList = ({ chatItems }: { chatItems: Array<any> }) => {
+  const currentTime = new Date().getTime();
+
   chatItems.forEach((ele) => {
     const diff = currentTime - ele.lastChat;
     const dayDiff = Math.floor(diff / (24 * 60 * 60));
-    const key = dayDiff <= 1 ? 1 : dayDiff <= 7 ? 7 : 30;
-    ele["key"] = key;
+    const key = dayDiff <= 1 
+    ? 1 
+    : dayDiff <= 7 ?
+     7 
+     : 30;
+    ele['key'] = key;
   });
-  const currentTime = new Date(). getTime();
 
-  return(
+  const dateGroups = [
+    { label: '어제', day: 1 },
+    { label: '지난 7일', day: 7 },
+    { label: '지난 30일', day: 30 },
+  ];
+
+  return (
     <div className='chatlist'>
-      <div className='yesterday'>
-      <ul>
-          {chatItems
-            .filter((item) => item.key === 1)
-            .map((chat) => (
-              <li key={chat.chatId}>{chat.title}</li>
-            ))}
-        </ul>
-      </div>
-      <div className='previous7Days'>
-        <span>지난 7일</span>
-        <ul>
-          {chatItems
-            .filter((item) => item.key === 7)
-            .map((chat) => (
-              <li key={chat.chatId}>{chat.title}</li>
-            ))}
-        </ul>
-      </div>
-      <div className='previous30Days'>
-        <span>지난 30일</span>
-        <ul>
-          {chatItems
-            .filter((item) => item.key === 30)
-            .map((chat) => (
-              <li key={chat.chatId}>{chat.title}</li>
-            ))}
-        </ul>
-      </div>
+      {dateGroups.map((group) => (
+        <div>
+          <span>{group.label}</span>
+          <ul>
+            {chatItems
+              .filter((item) => item.key === group.day)
+              .map((chat) => (
+                <li key={chat.chatId}>{chat.title}</li>
+              ))}
+          </ul>
+        </div>
+      ))}
     </div>
-    );
-  }
+  );
+};
 export default ChatList;
--- 2-4weaks_study/sidebar/src/Components/Sidebar.tsx
+++ 2-4weaks_study/sidebar/src/Components/Sidebar.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import '../App.css';
 import { useState } from 'react';
 import profileImg from '../profile_image.jpg';
 import { Icons } from '../Icons';
 import ChatList from '../ChatList';
 
-
+const defaultChatItems = [
+  {
+    title: 'react에서 list 렌더링하는법',
+    chatId: '3',
+    lastChat: new Date().getTime() - 0,
+  },
+  {
+    title: 'react 장점',
+    chatId: '2',
+    lastChat: new Date().getTime() - 3600 * 24 * 3,
+  },
+  {
+    title: 'react JSX',
+    chatId: '1',
+    lastChat: new Date().getTime() - 3600 * 24 * 8,
+  },
+  {
+    title: 'react 프레임워크',
+    chatId: '0',
+    lastChat: new Date().getTime() - 3600 * 24 * 9,
+  },
+];
 
 const Sidebar = () => {
   const [isPopupVisible, setPopupVisible] = useState(false);
@@ -14,12 +35,29 @@ const Sidebar = () => {
     setPopupVisible(!isPopupVisible);
   };
 
+  // 변하는 값에 대해서 값에 따라 컴포넌트를 리렌더하기 위해 이용한다.
+  // 상세한 useState 동작 방식에 대해선 "클로저" 라는 개념을 이해하면 된다.
+  const [chatItems, setChatItems] = useState(defaultChatItems);
+
+  // const chatItems = chatState[0]
+  // const setChatItems = chatState[1]
+  const onCreateChat = useCallback(() => {
+    setChatItems((prev) => [
+      {
+        title: '새로운 채팅',
+        chatId: prev.length.toString(),
+        lastChat: new Date().getTime() - 0,
+      },
+      ...prev,
+    ]);
+  }, []);
+
   return (
     <div>
       <div className='top-sidebar'>
         <h1>채팅</h1>
         <div className='top-sidebar-btn'>
-          <button className='newchat-btn'>
+          <button className='newchat-btn' onClick={onCreateChat}>
             <span>{Icons.add}</span>
             새로운 채팅
           </button>
@@ -33,22 +71,23 @@ const Sidebar = () => {
       </div>
 
       <div className='past-content'>
-        {/*<ChatList />*/}
+        <ChatList chatItems={chatItems} />
       </div>
-        
+
       <div className='profile-menu' onClick={handleButtonClick}>
+        <div className='popup-wrapper'>
+          {isPopupVisible && (
+            <div className='popup'>
+              <div className='menu-setting'>{Icons.setting}설정</div>
+              <div className='menu-logout'>{Icons.logout}로그아웃</div>
+            </div>
+          )}
+        </div>
         <div className='profile'>
           <img className='profile-image' src={profileImg} alt='profile-image' />
           <span className='userName'>황소정</span>
         </div>
       </div>
-
-      {isPopupVisible && (
-        <div className='popup'>
-          <div className='menu-setting'>{Icons.setting}설정</div>
-          <div className='menu-logout'>{Icons.logout}로그아웃</div>
-        </div>
-      )}
     </div>
   );
 };
